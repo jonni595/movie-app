39f874a73b376dc2fec35183c64249244f7cb4d4
Jonathan <jonathandajome@gmail.com>
2024-10-29 21:40:34 -0500

    add global styles

---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: "system-ui, sans-serif",
+    monospace: "Menlo, monospace",
+  },
+};
