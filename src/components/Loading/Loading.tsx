92aeab10da7be1ed49eba73fa6f9f19131de5dab
Jonathan <jonathandajome@gmail.com>
2024-10-30 03:16:38 -0500

    add loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,30 @@
+import { Loader } from "./Loading.style";
+
+const Loading = () => {
+  return (
+    <Loader>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Loader>
+  );
+};
+
+export { Loading };
