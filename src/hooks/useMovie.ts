f8fa1120d2483af481d801740ddc307642c9a207
Jonathan <jonathandajome@gmail.com>
2024-10-30 03:18:27 -0500

    create custom hook

---
+++ src/hooks/useMovie.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import type { IMovie } from "../interface";
+import { API_URL } from "../config";
+
+export const useMovie = (initialValue: number) => {
+  const [movie, setMovie] = useState<IMovie | null>(null),
+    [error, setError] = useState<string | null>(null),
+    [loading, setLoading] = useState(true),
+    [page, setPage] = useState(initialValue);
+
+  const getMovies = async (count?: number) => {
+    try {
+      const response = await fetch(`${API_URL}${count}`);
+      const data = await response.json();
+      setMovie(data);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getMovies(page);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [page]);
+
+  return { movie, error, loading, page, handleNextPage };
+};
---
+++ src/interface/index.ts
@@ -0,0 +1,31 @@
+export interface IMovie {
+  page: number;
+  results: IMovieResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface IMovieResult {
+  adult?: boolean;
+  backdrop_path?: string;
+  genre_ids?: number[];
+  id: number;
+  original_language?: string;
+  original_title?: string;
+  overview: string;
+  popularity?: number;
+  poster_path: string;
+  release_date?: string;
+  title: string;
+  video?: boolean;
+  vote_average: number;
+  vote_count?: number;
+}
+
+export interface MainProps {
+  children: React.ReactNode;
+}
+
+export interface CardProps {
+  movie: IMovieResult;
+}
